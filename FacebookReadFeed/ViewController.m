cdcd175e126cc029dd41ea13fbf5f1a845ca87fd
saerem21 <nemessisdds>
2014-01-21 16:34:39 +0900

    피드 받아오는 예제입니다.
    잘

--- FacebookReadFeed/ViewController.m
+++ FacebookReadFeed/ViewController.m
@@ -7,12 +7,144 @@
 //
 
 #import "ViewController.h"
+#import <Social/Social.h>
+#import <Accounts/Accounts.h>
 
-@interface ViewController ()
+#define FACEBOOK_APPID @"344325922372478"
 
+
+@interface ViewController ()<UITableViewDataSource,UITableViewDelegate>
+
+@property (strong,nonatomic)NSArray *data;
+@property (weak, nonatomic) IBOutlet UITableView *table;
+@property (strong,nonatomic)ACAccount *facebookAccount;
 @end
 
 @implementation ViewController
+
+
+- (void)showTimeLine
+{
+    ACAccountStore *store = [[ACAccountStore alloc] init];
+    ACAccountType *accountType = [store accountTypeWithAccountTypeIdentifier:ACAccountTypeIdentifierFacebook];
+    NSDictionary *options = @{ACFacebookAppIdKey:FACEBOOK_APPID,
+                              ACFacebookPermissionsKey:@[@"read_stream"],
+                              ACFacebookAudienceKey:ACFacebookAudienceEveryone};
+    [store requestAccessToAccountsWithType:accountType options:options completion:^(BOOL granted, NSError *error) {
+        if (error) {
+            NSLog(@"Error:%@", error);
+        }
+        if (granted) {
+            NSLog(@"권한 승인 성공");
+            NSArray *accountList = [store accountsWithAccountType:accountType];
+            self.facebookAccount = [accountList lastObject];
+            
+            [self requestFeed];
+        }
+        else
+        {
+            NSLog(@"권한 승인 실패");
+        }
+    }];
+}
+
+
+//- (void)showTimeLine{
+//    ACAccountStore *store = [[ACAccountStore alloc] init];
+//    ACAccountType *accountType = [store accountTypeWithAccountTypeIdentifier:ACAccountTypeIdentifierFacebook];
+//    
+//    
+//    NSDictionary *options = @{ACFacebookAppIdKey: FACEBOOK_APPID,
+//                              ACFacebookPermissionsKey: @[@"read_stream"],
+//                              ACFacebookAudienceKey:ACFacebookAudienceEveryone};
+//    [store requestAccessToAccountsWithType:accountType options:options completion:^(BOOL granted,NSError *error){
+//        if(error){
+//            NSLog(@"error %@",error);
+//        }
+//        if(granted){
+//            NSArray *accounts = [store accountsWithAccountType:accountType];
+//            self.facebookAccount = [accounts lastObject];
+//            [self requestFeed];
+//        }
+//        else{
+//            NSLog(@"fall %@",error);
+//        }
+//    }];
+//}
+
+
+//- (void)requestFeed
+//{
+//    NSString *urlStr = @"https://graph.facebook.com/me/feed";
+//    NSURL *url = [NSURL URLWithString:urlStr];
+//    NSDictionary *params = nil;
+//    SLRequest *request = [SLRequest requestForServiceType:SLServiceTypeFacebook requestMethod:SLRequestMethodGET URL:url parameters:params];
+//    request.account = self.facebookAccount;
+//    
+//    [request performRequestWithHandler:^(NSData *responseData, NSHTTPURLResponse *urlResponse, NSError *error) {
+//        if (error != nil) {
+//            NSLog(@"Error : %@", error);
+//            return;
+//        }
+//        
+//        __autoreleasing NSError *parseError;
+//        NSDictionary *result = [NSJSONSerialization JSONObjectWithData:responseData options:kNilOptions error:&parseError];
+//        
+//        self.data = result[@"data"];
+//        [[NSOperationQueue mainQueue] addOperationWithBlock:^{
+//            [self.table reloadData];
+//        }];
+//    }];
+//}
+
+
+- (void)requestFeed{
+    NSURL *url = [NSURL URLWithString:@"https://graph.facebook.com/me/feed"];
+    NSDictionary *param = nil;
+    SLRequest *request = [SLRequest requestForServiceType:SLServiceTypeFacebook requestMethod:SLRequestMethodGET URL:url parameters:param];
+    request.account = self.facebookAccount;
+    
+    [request performRequestWithHandler:^(NSData *responseData,NSHTTPURLResponse *urlResponse,NSError *error){
+        if(nil != error){
+            NSLog(@"실패 %@",error);
+            return ;
+        }
+        
+        __autoreleasing NSError *parseError = nil;
+        NSDictionary *result = [NSJSONSerialization JSONObjectWithData:responseData options:kNilOptions error:&parseError];
+        self.data = result[@"data"];
+        [[NSOperationQueue mainQueue] addOperationWithBlock:^{
+            [self.table reloadData];
+        }];
+    }];
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return [self.data count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"CELL"];
+    NSDictionary *one = self.data[indexPath.row];
+    
+    NSString *contents;
+    if(one[@"message"]){
+        NSDictionary *likes = one[@"likes"];
+        NSArray *data = likes[@"data"];
+       // NSLog(@"message likes %@ - %@",likes,count);
+        contents = [NSString stringWithFormat:@" %@ ...(%d)",one[@"message"],[data count]];
+    }
+    else{
+        contents = one[@"story"];
+        cell.indentationLevel = 2;
+    }
+    cell.textLabel.text = contents;
+    
+    return cell;
+}
+- (void)viewWillAppear:(BOOL)animated{
+    [self showTimeLine];
+}
 
 - (void)viewDidLoad
 {
